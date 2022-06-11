f4f428ea6df2ba0b2bd064368f3a319667820223
NikiGorbunov <97124792+NikiGorbunov@users.noreply.github.com>
2022-06-11 22:10:24 +0300

    main

---
+++ RickAndMorty/Models/RickAndMorty.swift
@@ -0,0 +1,8 @@
+//
+//  RickAndMorty.swift
+//  RickAndMorty
+//
+//  Created by Никита Горбунов on 10.06.2022.
+//
+
+import Foundation
---
+++ RickAndMorty/Services/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  RickAndMorty
+//
+//  Created by Никита Горбунов on 10.06.2022.
+//
+
+import Foundation
