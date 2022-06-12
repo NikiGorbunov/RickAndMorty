6a4156d68a4b4fa9257f8db12797a7300dfaf1db
NikiGorbunov <97124792+NikiGorbunov@users.noreply.github.com>
2022-06-12 22:24:21 +0300

    Model

--- RickAndMorty/Models/RickAndMorty.swift
+++ RickAndMorty/Models/RickAndMorty.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+struct RickAndMorty: Decodable {
+    let info: Info
+    let results: [Character]
+}
+
+struct Info: Decodable {
+    let pages: Int
+    let next: String?
+    let prev: String?
+}
+
+struct Character: Decodable {
+    let id: Int
+    let name: String
+    let status: String
+    let species: String
+    let gender: String
+    let origin: Location
+    let location: Location
+    let image: String
+    let episode: [String]
+    let url: String
+    
+    var description: String {
+        """
+    Name: \(name)
+    Status: \(status)
+    Species: \(species)
+    Gender: \(gender)
+    Origin: \(origin.name)
+    Location: \(location.name)
+    """
+        
+    }
+}
+
+struct Location: Decodable {
+    let name: String
+}
+
+enum Link: String {
+    case rickAndMortyApi = "https://rickandmortyapi.com/api/character"
+}
+
+
