007f0f41b02ac2b1a79c7e1ef6eab38eaa107db3
NikiGorbunov <97124792+NikiGorbunov@users.noreply.github.com>
2022-06-15 20:08:36 +0300

    NetworkManager

--- RickAndMorty/Models/RickAndMorty.swift
+++ RickAndMorty/Models/RickAndMorty.swift
@@ -13,9 +13,10 @@ struct RickAndMorty: Decodable {
 }
 
 struct Info: Decodable {
+    let count: Int?
     let pages: Int?
-    let next: String?
     let prev: String?
+    let next: String?
 }
 
 struct Character: Decodable {
@@ -23,46 +24,17 @@ struct Character: Decodable {
     let name: String?
     let status: String?
     let species: String?
+    let type: String?
     let gender: String?
     let origin: Location?
     let location: Location?
     let image: String?
     let episode: [String]?
-    let url: String?
-    
-    init(characterData: [String:Any]) {
-        self.id = characterData["id"] as? Int
-        self.name = characterData["name"] as? String
-        self.status = characterData["status"] as? String
-        self.species = characterData["species"] as? String
-        self.gender = characterData["gender"] as? String
-        self.origin = characterData["origin"] as? Location
-        self.location = characterData["location"] as? Location
-        self.image = characterData["image"] as? String
-        self.episode = characterData["episode"] as? [String]
-        self.url = characterData["url"] as? String
-    }
-        
-    static func getCharacters(from data: Any) -> [Character] {
-        guard let rickData = data as? [String: Any] else { return [] }
-        guard let results = rickData["results"] as? [[String: Any]] else { return [] }
-        
-        var characters = [Character]()
-        
-        for result in results {
-            let character = Character(characterData: result)
-            characters.append(character)
-        }
-        return characters
-    }
 }
 
 struct Location: Decodable {
     let name: String?
-}
-
-enum Link: String {
-    case rickAndMortyApi = "https://rickandmortyapi.com/api/character"
+    let url: String?
 }
 
 
--- RickAndMorty/Services/NetworkManager.swift
+++ RickAndMorty/Services/NetworkManager.swift
@@ -5,17 +5,52 @@
 //  Created by Никита Горбунов on 10.06.2022.
 //
 
-import Foundation
 import Alamofire
+import AlamofireImage
 
+protocol APIManagerProtocol {
+    func sendRequest(url: URL, completion: @escaping(_ characters: [Character]) -> ())
+}
 
-class NetworkManager {
-    //Singelton
-    static let shared = NetworkManager()
-    private init() {}
+class APIManager: APIManagerProtocol {
+    private var data: Data?
     
-    func fetchData(url: String, completion: @escaping (Result<RickAndMorty, Error>) -> Void) {
-        AF.request(url)
-            .validate()
+    func sendRequest(url: URL, completion: @escaping ([Character]) -> ()) {
+        AF.request(url, method: .get).responseDecodable(of: RickAndMorty.self) { response in
+            
+            switch response.result {
+            case .success(let characters):
+                
+                var results = [Character]()
+                characters.results?.forEach { elements in
+                    let character = Character(id: elements.id,
+                                              name: elements.name,
+                                              status: elements.status,
+                                              species: elements.species,
+                                              type: elements.type,
+                                              gender: elements.gender,
+                                              origin: elements.origin,
+                                              location: elements.location,
+                                              image: elements.image,
+                                              episode: elements.episode)
+                    results.append(character)
+                }
+                completion(results)
+            case .failure(let error):
+                NSLog("\(error)")
+            }
+        }
+    }
+    
+    
+    func getImage(_ url: URL, completion: @escaping (UIImage?) -> Void) {
+        AF.request(url, method: .get).responseImage { response in
+            switch response.result {
+            case .success(let img):
+                completion(img)
+            case .failure(let error):
+                NSLog("\(error)")
+            }
+        }
     }
 }
